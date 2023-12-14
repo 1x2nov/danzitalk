37996cfd6091f3296cf6d761ca542f904ef87da2
1x2nov <108854175+1x2nov@users.noreply.github.com>
2023-12-14 16:09:58 +0900

    Delete reality directory

--- reality/templatetags/index.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def index(indexable, i):
-    try:
-        return indexable[i]
-    except:
-        return False
--- reality/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from community import views
-from .views import Search, ToggleBookmark
-urlpatterns = [
-    path('search', Search.as_view()),
-    path('<int:reality_id>/bookmark', ToggleBookmark.as_view()),
-]
